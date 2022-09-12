8e20c08df39aef70f6aef618f562210100a4cd05
Mariia Zav <mariiazavarzina@gmail.com>
2022-09-12 14:03:18 +0200

    project base page

---
+++ python_tests/pages/base_page.py
@@ -0,0 +1,68 @@
+# вспомогательные методы для работы с драйвером
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+from enums.global_enums import GlobalErrorMessages
+import math
+
+
+class BasePage:
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def go_to_basket(self):
+        basket_link = self.browser.find_element(*BasePageLocators.BUTTON_GO_TO_BASKET)
+        basket_link.click()
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def is_element_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    def is_element_not_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_link_to_the_basket(self):
+        assert self.is_element_present(*BasePageLocators.BUTTON_GO_TO_BASKET), GlobalErrorMessages.BUTTON_MISSING
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), GlobalErrorMessages.LINK_MISSING.value
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
+
