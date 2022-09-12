005aec8852c005a4f64c23efd204ef4e5a6884c5
Mariia Zav <mariiazavarzina@gmail.com>
2022-09-12 13:37:29 +0200

    added config file

---
+++ configuration.py
@@ -0,0 +1,11 @@
+# переменные, относящиеся к нашим тестам
+
+SERVICE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
+
+LOGIN_URL = "http://selenium1py.pythonanywhere.com/"
+
+CATALOGUE_URL = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+CATALOGUE_URL_SEC_ITEM = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+
