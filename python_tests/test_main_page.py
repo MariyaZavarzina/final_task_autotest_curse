4f23c198220214f55777f3f77cdc85133f26c809
Mariia Zav <mariiazavarzina@gmail.com>
2022-09-19 16:49:29 +0200

    moved the method to the test product page

--- python_tests/test_main_page.py
+++ python_tests/test_main_page.py
@@ -4,21 +4,20 @@ from configuration import *
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
-from .pages.product_page import ProductPage
 
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, LOGIN_URL)
-    page.open()
-    page.go_to_login_page()
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, LOGIN_URL)
+        page.open()
+        page.go_to_login_page()
 
-@pytest.mark.skip
-def test_should_be_login_link(browser):
-    page = MainPage(browser, LOGIN_URL)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, LOGIN_URL)
+        page.open()
+        page.should_be_login_link()
 
 
 """def test_verification_link(browser): # 1-ый вариант перехода между страницами
