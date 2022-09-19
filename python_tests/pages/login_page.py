4d307854c1b96b3752a45390a5da71215274442a
Mariia Zav <mariiazavarzina@gmail.com>
2022-09-19 16:47:06 +0200

    user registration method added

--- python_tests/pages/login_page.py
+++ python_tests/pages/login_page.py
@@ -1,9 +1,15 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 from enums.global_enums import *
+from random_word import RandomWords
+
+r = RandomWords()
 
 
 class LoginPage(BasePage):
+    def __init__(self, browser, url):
+        super().__init__(browser, url)
+
     def should_be_login_page(self):
         self.should_be_substring()
         self.should_be_login_form()
@@ -17,3 +23,32 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), GlobalFormMessages.REGISTER_FORM_MISSING.value
+
+    def register_new_user(self):
+        self.random_email()
+        self.random_password()
+        self.press_button_to_register()
+
+    def random_email(self):
+        email = r.get_random_word() + "@fakemail.org"
+        user_login = self.browser.find_element(*LoginPageLocators.USER_LOGIN)
+        user_login.send_keys(email)
+
+
+    def random_password(self):
+        password = r.get_random_word() + "123"
+        user_password = self.browser.find_element(*LoginPageLocators.USER_PASSWORD)
+        user_password.send_keys(password)
+        user_password = self.browser.find_element(*LoginPageLocators.VERIFICATION_USER_PASSWORD)
+        user_password.send_keys(password)
+
+    def press_button_to_register(self):
+        button = self.browser.find_element(*LoginPageLocators.BUTTON_REGISTER)
+        button.click()
+
+
+
+
+
+
+
